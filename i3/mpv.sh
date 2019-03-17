9791bc1b0ab9725672d8a7013e577a915c14bacc
vinicius molina <vinicius_molina99@hotmail.com>
2019-03-17 10:25:54 -0300

    yt function refactory / mpv with ytdl corrections

--- i3/mpv.sh
+++ i3/mpv.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+
 pkill -f animatedWallpaper &
 wait
 pkill -f feh
-exec -a animatedWallpaper xwinwrap -fs -ni -s -ov -- mpv -wid WID $1
+feh --bg-fill "$SCRIPTPATH/black-background.jpg"
+exec -a animatedWallpaper xwinwrap -fs -ni -s -ov -- mpv -wid WID --ytdl-format=mp4 $1
